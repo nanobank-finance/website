462f6357d9f851aba90863edd166a5bb47325cdf
Nate Schultz <ngs32@drexel.edu>
2022-11-28 21:38:02 -0500

    worked on footer

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -13,7 +13,8 @@ import {
     Footer
 } from 'grommet';
 import '../App.css';
-import { Test, Money, Currency, Home } from 'grommet-icons';
+import { Test, Money, Currency, Home, Github, Twitter, Reddit, History, CloudUpload } from 'grommet-icons';
+import { FaDiscord } from 'react-icons/fa';
 import { GoogleLogin } from '@react-oauth/google';
 import { Navigate, useNavigate } from "react-router-dom";
 import { useEffect, useState } from 'react';
@@ -23,6 +24,7 @@ const theme = {
         colors: {
             brand: '#FFFFFF',
             // background: '#809bce',
+            footer: 'lightgray',
             placeholder: '#000000',
             good: '#37eb34',
         },
@@ -58,6 +60,17 @@ function decodeJwtResponse(token: string) {
     return JSON.parse(jsonPayload);
 };
 
+function ListItem(text: string, icon: any) {
+    return (
+    <Box fill margin="large">
+        <Box alignSelf="center">{icon}</Box>
+        <Text size="xl" margin={{vertical: "large", horizontal: "large"}} alignSelf="center">
+            {text}
+        </Text>
+    </Box>
+    );
+}
+
 function LandingPage() {
     var filename = "DALL·E 2022-11-28 18.50.29 - photorealistic nature.png";
     return (
@@ -81,42 +94,34 @@ function LandingPage() {
                     </Text>
                     <Button focusIndicator={true} margin="large" label="login / signup" alignSelf='end'/>
             </Header>
-                <Box direction='row' margin={{horizontal: "large"}} height="10%" width="90%" background={"url('./"+filename+"')"}>
+                <Box direction='row' alignSelf="center" margin={{horizontal: "large"}} height="10%" width="90%" background={"url('./"+filename+"')"}>
                 </Box>
                 <Text size="2xl" margin="large" alignSelf="center">join a modern crypto bank</Text>
-                <Box direction="row">
-                    <Box fill>
-                        <Box margin="medium">
-                        <Box alignSelf="center"><Test /></Box>
-                        <Text size="xl" margin={{vertical: "medium", horizontal: "medium"}} alignSelf="center">
-                            earn interest on your XNO
-                        </Text>
-                        </Box>
-                        <Box margin="medium">
-                        <Box alignSelf="center"><Home /></Box>
-                        <Text size="xl" margin={{vertical: "medium", horizontal: "medium"}} alignSelf="center">
-                            pay your mortgage in XNO
-                        </Text>
-                        </Box>
+                <Box fill direction="row">
+                    {ListItem("earn interest on your XNO", <Test />)}
+                    {ListItem("take out a loan in XNO", <Home />)}
+                </Box><Box fill direction="row">
+                    {ListItem("insure your XNO", <CloudUpload />)}
+                    {ListItem("schedule regular transfers", <History />)}
+                </Box><Box fill direction="row">
+                    {ListItem("no surprise fees", <Money />)}
+                    {ListItem("buy XNO with USD", <Currency />)}
+                </Box>
+                <Box direction="row" margin="small">
+                    <Box fill direction="row" as="footer">
+                        <Button tip="github" margin={{horizontal: "small"}} alignSelf="end"><Github></Github></Button>
+                        <Button tip="twitter" margin={{horizontal: "small"}} alignSelf="end"><Twitter></Twitter></Button>
+                        <Button tip="reddit" margin={{horizontal: "small"}} alignSelf="end"><Reddit></Reddit></Button>
+                        <Button tip="discord" margin={{horizontal: "small"}} alignSelf="end"><FaDiscord></FaDiscord></Button>
                     </Box>
-                    <Box fill>
-                        <Box margin="medium">
-                        <Box alignSelf="center"><Money /></Box>
-                        <Text size="xl" margin={{vertical: "medium", horizontal: "medium"}} alignSelf="center">
-                            take out a personal loan in XNO
-                        </Text>
-                        </Box>
-                        <Box margin="medium">
-                        <Box alignSelf="center"><Currency /></Box>
-                        <Text size="xl" margin={{vertical: "medium", horizontal: "medium"}} alignSelf="center">
-                            buy XNO with USD
-                        </Text>
-                        </Box>
+                    <Box fill></Box>
+                    <Box fill></Box>
+                    <Box fill direction="row" as="footer">
+                        <Anchor color="#000000" alignSelf="end" margin={{horizontal:"small"}}><Text>About</Text></Anchor>
+                        <Anchor color="#000000" alignSelf="end" margin={{horizontal:"small"}}><Text>Careers</Text></Anchor>
+                        <Anchor color="#000000" alignSelf="end" margin={{horizontal:"small"}}><Text>Legal</Text></Anchor>
                     </Box>
                 </Box>
-            <Box direction="row">
-                <Text></Text>
-            </Box>
         </Layer>
         </Grommet>
     );
