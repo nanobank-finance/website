46c397815545665668fa066e8533437888f12042
Nate Schultz <ngs32@drexel.edu>
2022-11-29 17:47:11 -0500

    added footer to login page

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -53,154 +53,6 @@ const theme = {
 const SMALL_SCREEN_SIZE = 700;
 const MEDIUM_SCREEN_SIZE = 1000;
 
-function decodeJwtResponse(token: string) {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-
-    return JSON.parse(jsonPayload);
-};
-
-function HeaderGroup(width: number) {
-    if(width < SMALL_SCREEN_SIZE) {
-        return (<Header background="brand">
-            <Text size="4xl" margin={{
-                    left: "small",
-                }} alignSelf="start">
-
-                <Image 
-                    margin={{
-                        vertical: "medium",
-                        left: "large",
-                        right: "small"
-                    }}
-                    src="./favicon-32x32.png"
-                />
-
-                nano bank
-            </Text>
-        </Header>)
-    } else {
-        return (<Header background="brand">
-            <Text size="4xl" margin={{
-                    left: "small",
-                }} alignSelf="start">
-
-                <Image 
-                    margin={{
-                        vertical: "medium",
-                        left: "large",
-                        right: "small"
-                    }}
-                    src="./favicon-32x32.png"
-                />
-
-                nano bank
-            </Text>
-            <Button focusIndicator={true} margin="large" label="login / signup" alignSelf='end'/>
-        </Header>)
-    }
-}
-
-function FooterGroup(width: number) {
-    if(width < SMALL_SCREEN_SIZE) {
-        return (
-            <Box direction="column" margin="small">
-                <Box direction="row" margin={{vertical: "small"}}>
-                    <Button tip="github" margin={{horizontal: "small"}} alignSelf="center"><Github></Github></Button>
-                    <Button tip="twitter" margin={{horizontal: "small"}} alignSelf="center"><Twitter></Twitter></Button>
-                    <Button tip="reddit" margin={{horizontal: "small"}} alignSelf="center"><Reddit></Reddit></Button>
-                    <Button tip="discord" margin={{horizontal: "small"}} alignSelf="center"><FaDiscord></FaDiscord></Button>
-                    <Button tip="medium" margin={{horizontal: "small"}} alignSelf="center"><Medium></Medium></Button>
-                </Box><Box direction="row" margin={{vertical: "small"}}>
-                    <Anchor color="#000000" alignSelf="center" margin={{horizontal:"small"}}><Text>About</Text></Anchor>
-                    <Anchor color="#000000" alignSelf="center" margin={{horizontal:"small"}}><Text>Careers</Text></Anchor>
-                    <Anchor color="#000000" alignSelf="center" margin={{horizontal:"small"}}><Text>Legal</Text></Anchor>
-                    <Box fill></Box>
-                    <Button focusIndicator={true} label="login / signup" alignSelf='end' margin="small"/>
-                </Box>
-                <Box direction="row" margin="small">
-                    <Anchor color="gray" alignSelf="center"><Text>Nano Bank LLC © {new Date().getFullYear()}</Text></Anchor>
-                </Box>
-            </Box>);
-    } else {
-        return (
-            <Box direction="column">
-                <Box direction="row" margin={{vertical: "small"}}>
-                    <Button tip="github" margin={{horizontal: "small"}} alignSelf="center"><Github></Github></Button>
-                    <Button tip="twitter" margin={{horizontal: "small"}} alignSelf="center"><Twitter></Twitter></Button>
-                    <Button tip="reddit" margin={{horizontal: "small"}} alignSelf="center"><Reddit></Reddit></Button>
-                    <Button tip="discord" margin={{horizontal: "small"}} alignSelf="center"><FaDiscord></FaDiscord></Button>
-                    <Button tip="medium" margin={{horizontal: "small"}} alignSelf="center"><Medium></Medium></Button>
-                </Box><Box direction="row" margin={{vertical: "small"}}>
-                    <Anchor color="#000000" alignSelf="center" margin={{horizontal:"small"}}><Text>About</Text></Anchor>
-                    <Anchor color="#000000" alignSelf="center" margin={{horizontal:"small"}}><Text>Careers</Text></Anchor>
-                    <Anchor color="#000000" alignSelf="center" margin={{horizontal:"small"}}><Text>Legal</Text></Anchor>
-                </Box>
-                <Box direction="row" margin="small">
-                    <Anchor color="gray" alignSelf="center"><Text>Nano Bank LLC © {new Date().getFullYear()}</Text></Anchor>
-                </Box>
-            </Box>);
-    }
-}
-
-function ListItem(text: string, icon: any, vert_margin: string, horiz_margin: string) {
-    return (
-    <Box fill margin="large">
-        <Box alignSelf="center">{icon}</Box>
-        <Text size="xl" margin={{vertical: vert_margin, horizontal: horiz_margin}} alignSelf="center">
-            {text}
-        </Text>
-    </Box>
-    );
-}
-
-function ItemGrid(width: number) {
-    if(width < SMALL_SCREEN_SIZE) {
-        // small screen / phone size
-        return (<Box fill direction='column' margin={{bottom: "large"}} overflow={{vertical: "scroll"}}>
-        <Box fill direction="row" margin={{vertical:"xlarge"}}>
-            {ListItem("earn interest on your XNO", <Test size="large"/>, "large", "large")}
-        </Box><Box fill direction="row" margin={{vertical:"xlarge"}}>
-            {ListItem("take out a loan in XNO", <Home size="large"/>, "large", "large")}
-        </Box><Box fill direction="row" margin={{vertical:"xlarge"}}>
-            {ListItem("insure your XNO", <CloudUpload size="large"/>, "large", "large")}
-        </Box><Box fill direction="row" margin={{vertical:"xlarge"}}>
-            {ListItem("schedule regular transfers", <History size="large"/>, "large", "large")}
-        </Box><Box fill direction="row" margin={{vertical:"xlarge"}}>
-            {ListItem("no surprise fees", <Money size="large"/>, "large", "large")}
-        </Box><Box fill direction="row" margin={{vertical:"xlarge"}}>
-            {ListItem("buy XNO with USD", <Currency size="large"/>, "large", "large")}
-        </Box>
-        </Box>);
-    } else if(width < MEDIUM_SCREEN_SIZE) {
-        // medium screen
-        return (<Box fill direction='column' margin={{bottom: "large"}} overflow={{vertical: "scroll"}}><Box fill direction="row">
-            {ListItem("earn interest on your XNO", <Test size="large"/>, "large", "large")}
-            {ListItem("take out a loan in XNO", <Home size="large"/>, "large", "large")}
-        </Box><Box fill direction="row">
-            {ListItem("insure your XNO", <CloudUpload size="large"/>, "large", "large")}
-            {ListItem("schedule regular transfers", <History size="large"/>, "large", "large")}
-        </Box><Box fill direction="row">
-            {ListItem("no surprise fees", <Money size="large"/>, "large", "large")}
-            {ListItem("buy XNO with USD", <Currency size="large"/>, "large", "large")}
-        </Box></Box>);
-    } else {
-        // large screen
-        return (<Box fill direction='column' margin={{bottom: "large"}} overflow={{vertical: "scroll"}}><Box fill direction="row">
-        {ListItem("earn interest on your XNO", <Test size="large"/>, "large", "small")}
-        {ListItem("take out a loan in XNO", <Home size="large"/>, "large", "small")}
-        {ListItem("insure your XNO", <CloudUpload size="large"/>, "large", "small")}
-        </Box><Box fill direction="row">
-        {ListItem("schedule regular transfers", <History size="large"/>, "large", "small")}
-        {ListItem("no surprise fees", <Money size="large"/>, "large", "small")}
-        {ListItem("buy XNO with USD", <Currency size="large"/>, "large", "small")}
-        </Box></Box>);
-    }
-}
-
 function LoginPage() {
     const filename = "DALL·E 2022-11-28 18.50.29 - photorealistic nature.png";
     const [width, setWidth] = useState<number>(() => {return 0});
@@ -230,6 +82,7 @@ function LoginPage() {
     return (
         <Grommet theme={theme}>
         <Layer full={true} modal={false} animate={false}>
+            {/* header */}
             <Header background="brand">
                 <Text size="4xl" margin={{
                         left: "small",
@@ -247,6 +100,29 @@ function LoginPage() {
                     nano bank
                 </Text>
             </Header>
+
+            {/* body */}
+            <Box fill>
+                
+            </Box>
+
+            {/* footer */}
+            <Box direction="column">
+                <Box direction="row" margin={{vertical: "small"}}>
+                    <Button tip="github" margin={{horizontal: "small"}} alignSelf="center"><Github></Github></Button>
+                    <Button tip="twitter" margin={{horizontal: "small"}} alignSelf="center"><Twitter></Twitter></Button>
+                    <Button tip="reddit" margin={{horizontal: "small"}} alignSelf="center"><Reddit></Reddit></Button>
+                    <Button tip="discord" margin={{horizontal: "small"}} alignSelf="center"><FaDiscord></FaDiscord></Button>
+                    <Button tip="medium" margin={{horizontal: "small"}} alignSelf="center"><Medium></Medium></Button>
+                </Box><Box direction="row" margin={{vertical: "small"}}>
+                    <Anchor color="#000000" alignSelf="center" margin={{horizontal:"small"}}><Text>About</Text></Anchor>
+                    <Anchor color="#000000" alignSelf="center" margin={{horizontal:"small"}}><Text>Careers</Text></Anchor>
+                    <Anchor color="#000000" alignSelf="center" margin={{horizontal:"small"}}><Text>Legal</Text></Anchor>
+                </Box>
+                <Box direction="row" margin="small">
+                    <Anchor color="gray" alignSelf="center"><Text>Nano Bank LLC © {new Date().getFullYear()}</Text></Anchor>
+                </Box>
+            </Box>
         </Layer>
         </Grommet>
     );
