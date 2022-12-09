fdc25ce2fe87deec3494600e1d0a4ef02cabdddb
Nate Schultz <nate.schultz@outlook.com>
2022-12-08 19:18:06 -0500

    starting over with a new ui template and new ui component library

--- README.md
+++ README.md
@@ -44,3 +44,11 @@ You don’t have to ever use `eject`. The curated feature set is suitable for sm
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
 To learn React, check out the [React documentation](https://reactjs.org/).
+
+## Credit
+
+### Paperbase Theme
+
+License: MIT
+
+https://github.com/mui/material-ui/tree/master/docs/src/pages/premium-themes/paperbase
---
+++ src/Navigator.tsx
@@ -0,0 +1,79 @@
+import * as React from 'react';
+import Divider from '@mui/material/Divider';
+import Drawer, { DrawerProps } from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Box from '@mui/material/Box';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import DnsRoundedIcon from '@mui/icons-material/DnsRounded';
+import PermMediaOutlinedIcon from '@mui/icons-material/PhotoSizeSelectActual';
+import PublicIcon from '@mui/icons-material/Public';
+import SettingsEthernetIcon from '@mui/icons-material/SettingsEthernet';
+import SettingsInputComponentIcon from '@mui/icons-material/SettingsInputComponent';
+import TimerIcon from '@mui/icons-material/Timer';
+import SettingsIcon from '@mui/icons-material/Settings';
+import PhonelinkSetupIcon from '@mui/icons-material/PhonelinkSetup';
+
+const categories = [
+  {
+    id: 'Build',
+    children: [
+      {
+        id: 'Dashboard',
+        icon: <PermMediaOutlinedIcon />,
+        active: true,
+      },
+      { id: 'Marketplace', icon: <PublicIcon /> },
+      { id: 'Wallets', icon: <DnsRoundedIcon /> },
+      { id: 'Account', icon: <SettingsIcon /> },
+      {
+        id: 'API Documentation',
+        icon: <SettingsEthernetIcon />,
+      },
+    ],
+  },
+];
+
+const item = {
+  py: '2px',
+  px: 3,
+  color: 'rgba(255, 255, 255, 0.7)',
+  '&:hover, &:focus': {
+    bgcolor: 'rgba(255, 255, 255, 0.08)',
+  },
+};
+
+const itemCategory = {
+  boxShadow: '0 -1px 0 rgb(255,255,255,0.1) inset',
+  py: 1.5,
+  px: 3,
+};
+
+export default function Navigator(props: DrawerProps) {
+  const { ...other } = props;
+
+  return (
+    <Drawer variant="permanent" {...other}>
+      <List disablePadding>
+        <ListItem sx={{ ...item, ...itemCategory, fontSize: 22, color: '#fff' }}>
+          Nano Swap
+        </ListItem>
+        {categories.map(({ id, children }) => (
+          <Box key={id} sx={{ bgcolor: '#101F33' }}>
+            {children.map(({ id: childId, icon, active }) => (
+              <ListItem disablePadding key={childId}>
+                <ListItemButton selected={active} sx={item}>
+                  <ListItemIcon>{icon}</ListItemIcon>
+                  <ListItemText>{childId}</ListItemText>
+                </ListItemButton>
+              </ListItem>
+            ))}
+            <Divider sx={{ mt: 2 }} />
+          </Box>
+        ))}
+      </List>
+    </Drawer>
+  );
+}
